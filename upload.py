309c51539593065375cdfd267eadbda0458a797c
Temprl Business <karthisk846@gmail.com>
2025-09-06 12:43:40 +0530

    file path

--- upload.py
+++ upload.py
@@ -1,11 +1,18 @@
 from langchain_community.document_loaders import PyPDFLoader
 # import os
 
-data_folder = "C:/Users/karth/OneDrive/Desktop/NLP/workshop_RAG/data/eng.pdf"
+data_folder = "./data/eng.pdf"
 
 loader = PyPDFLoader(data_folder)
-docs = loader.load()
+data = loader.load_and_split()
 
 # print(docs[0])
 
+# from qdrant_client import QdrantClient
 
+# qdrant_client = QdrantClient(
+#     url="https://9731d9ce-cbf4-4495-b840-9b220a8cd953.us-west-1-0.aws.cloud.qdrant.io:6333", 
+#     api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.QFclDuzPWVT9SlZ3V1nANM2FfeCEm-Vn9aMzCdgdrfQ",
+# )
+
+# print(qdrant_client.get_collections())
